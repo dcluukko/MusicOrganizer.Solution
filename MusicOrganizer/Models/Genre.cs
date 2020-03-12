de8a0345422a52256a05a72f27df53ede97bff85
Dana Luukko and Michelle Morin <example@example.com>
2020-03-12 10:49:56 -0700

    create methods GetAll - gets instances of genre, Find - find instances of genre by id, AddRecord - associates object record with object genre

--- MusicOrganizer/Models/Genre.cs
+++ MusicOrganizer/Models/Genre.cs
@@ -10,16 +10,29 @@ namespace MusicOrganizer.Models
     public int Id { get; }
     public List<Record> Records { get; set; }
 
-  public Genre(string genreType)
-  {
-    Type = genreType;
-    _genres.Add(this);
-    Id = _genres.Count;
-    Records = new List<Record> { };
-  }
-  public static void ClearAll()
-  {
-    _genres.Clear();
-  }
+    public Genre(string genreType)
+    {
+      Type = genreType;
+      _genres.Add(this);
+      Id = _genres.Count;
+      Records = new List<Record> { };
+    }
+    public static void ClearAll()
+    {
+      _genres.Clear();
+    }
+    public static List<Genre> GetAll()
+    {
+      return _genres;
+    }
+    public static Genre Find(int id)
+    {
+     return _genres[id - 1];
+    }
+
+    public void AddRecord(Record record)
+    {
+      Records.Add(record);
+    }
   }
 }
