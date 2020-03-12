7916aeba90572f9da3bd22c2d8ae643cc7c0a53e
Dana Luukko and Michelle Morin <example@example.com>
2020-03-12 11:04:28 -0700

    add record model methods

--- MusicOrganizer/Models/Record.cs
+++ MusicOrganizer/Models/Record.cs
@@ -6,12 +6,30 @@ namespace MusicOrganizer.Models
   {
     public string Artist { get; set; }
     public string Title { get; set; }
+    public int Id { get; }
+    private static List<Record> _records = new List<Record> { };
 
     public Record( string artist, string title )
     {
       Artist = artist;
       Title = title;
+      _records.Add(this);
+      Id = _records.Count;
+    }
+
+    public static List<Record> GetAll()
+    {
+      return _records;
+    }
+
+    public static void ClearAll()
+    {
+      _records.Clear();
+    }
+
+    public static Record Find(int id)
+    {
+      return _records[id - 1];
     }
   }
-
 }
